ca5de43e7b9ca785636354a2c5d6f88bba257c24
t3kt <tekt@immerse.studio>
2021-01-18 22:48:04 -0800

    added componentLoader

---
+++ src/editor/componentLoader/componentLoader.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from typing import Optional
+
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+class ComponentLoader:
+	def __init__(self, ownerComp: 'COMP'):
+		self.ownerComp = ownerComp
+
+	@property
+	def Component(self) -> 'Optional[COMP]':
+		for o in self.ownerComp.children:
+			return o
+
+	@property
+	def ComponentTox(self) -> 'Optional[str]':
+		comp = self.Component
+		return comp and comp.par.externaltox.eval()
+
+	@property
+	def ComponentName(self) -> 'Optional[str]':
+		comp = self.Component
+		return comp and comp.name
+
+	def LoadComponent(self, tox: str, name: str = None):
+		self.UnloadComponent()
+		comp = self.ownerComp.loadTox(tox)
+		if not name:
+			path = Path(tox)
+			name = path.stem
+		comp.name = name
+
+	def UnloadComponent(self):
+		for o in self.ownerComp.children:
+			if not o.valid:
+				continue
+			# noinspection PyBroadException
+			try:
+				o.destroy()
+			except:
+				pass
