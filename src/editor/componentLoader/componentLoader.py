6ea90249437b535447f1d1880b4829c054f4dfd9
t3kt <tekt@immerse.studio>
2021-01-24 12:03:46 -0800

    support both Path and str tox paths and other loader enhancements
    see #8

--- src/editor/componentLoader/componentLoader.py
+++ src/editor/componentLoader/componentLoader.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Type, Union
 
 # noinspection PyUnreachableCode
 if False:
@@ -26,13 +26,36 @@ class ComponentLoader:
 		comp = self.Component
 		return comp and comp.name
 
-	def LoadComponent(self, tox: str, name: str = None):
+	def LoadComponent(self, tox: 'Union[str, Path]', name: str = None) -> 'COMP':
 		self.UnloadComponent()
-		comp = self.ownerComp.loadTox(tox)
+		convertedPath = _convertPath(tox)
+		comp = self.ownerComp.loadTox(convertedPath)
 		if not name:
 			path = Path(tox)
 			name = path.stem
 		comp.name = name
+		comp.par.externaltox = convertedPath
+		return comp
+
+	def CreateNewComponent(
+			self, tox: 'Union[str, Path]', name: str = None,
+			template: 'Optional[COMP]' = None,
+			compType: 'Optional[Type[COMP]]' = None,
+			autoSave=True) -> 'COMP':
+		self.UnloadComponent()
+		if not name:
+			path = Path(tox)
+			name = path.stem
+		if template:
+			comp = self.ownerComp.copy(template, name=name, includeDocked=False)
+		else:
+			comp = self.ownerComp.create(compType or baseCOMP, name=name)
+		comp.name = name
+		convertedPath = _convertPath(tox)
+		comp.par.externaltox = convertedPath
+		if autoSave:
+			comp.save(convertedPath, createFolders=True)
+		return comp
 
 	def UnloadComponent(self):
 		for o in self.ownerComp.children:
@@ -46,3 +69,8 @@ class ComponentLoader:
 
 	def Unloadcomponent(self, _=None):
 		self.UnloadComponent()
+
+def _convertPath(path: 'Union[str, Path]'):
+	if isinstance(path, Path):
+		return path.as_posix()
+	return path
