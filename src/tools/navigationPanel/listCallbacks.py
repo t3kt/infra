405a562e155a2cc50ed626ef030baa698f6926c8
t3kt <tekt@immerse.studio>
2021-08-01 23:47:31 -0700

    added navigationPanel

---
+++ src/tools/navigationPanel/listCallbacks.py
@@ -0,0 +1,17 @@
+﻿from infraCommon import navigateTo
+
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+def onClick(info):
+	print(info)
+	data = info['rowData']
+	path = data['rowObject']['path']
+	navigateTo(op(path))
+	
+
+
+
+
