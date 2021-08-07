b7bb752f8ab512efe46c5f985a04e5d172810d7e
t3kt <tekt@immerse.studio>
2021-08-07 16:20:20 -0700

    starting the perform library

---
+++ src/perform/hosting/performTrack/performTrack.py
@@ -0,0 +1,8 @@
+# noinspection PyUnreachableCode
+if False:
+	# noinspection PyUnresolvedReferences
+	from _stubs import *
+
+class PerformTrack:
+	def __init__(self, ownerComp: 'COMP'):
+		self.ownerComp = ownerComp
