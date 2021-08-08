0c35b986a6be417654ca121e1ef6d4a3f938a9cd
t3kt <tekt@immerse.studio>
2021-08-07 23:50:02 -0700

    track loading and unloading

--- src/perform/hosting/performTrack/performTrack.py
+++ src/perform/hosting/performTrack/performTrack.py
@@ -2,7 +2,23 @@
 if False:
 	# noinspection PyUnresolvedReferences
 	from _stubs import *
+	from components.hosting.componentLoader.componentLoader import ComponentLoader
+	iop.loader = ComponentLoader(COMP())
 
 class PerformTrack:
 	def __init__(self, ownerComp: 'COMP'):
 		self.ownerComp = ownerComp
+
+	def promptLoadTox(self):
+		tox = ui.chooseFile(
+			load=True,
+			fileTypes=['tox'],
+			title=f'Load scene for {self.ownerComp.par.Header}',
+		)
+		if tox:
+			iop.loader.LoadComponent(tox)
+
+	@staticmethod
+	def unloadTox():
+		iop.loader.UnloadComponent()
+
