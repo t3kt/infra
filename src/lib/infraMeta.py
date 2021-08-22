89f19296ad4458666a47966179f61f8a6dac31d9
t3kt <tekt@immerse.studio>
2021-08-22 16:13:04 -0700

    making component meta utils work when meta comps aren't present

--- src/lib/infraMeta.py
+++ src/lib/infraMeta.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional, Union, List
+from typing import Optional, Union, List, Tuple
 
 from infraCommon import mergeDicts
 from infraData import DataObjectBase
@@ -45,6 +45,7 @@ class CompInfo:
 	comp: 'Optional[AnyOpT]'
 	metaComp: 'Optional[_CompMetaCompT]'
 	metaPar: 'Optional[_CompMetaParsT]'
+	_libInfo: 'Optional[LibraryInfo]'
 
 	def __init__(self, o: 'Union[OP, str, Cell, Par]'):
 		o = op(o)
@@ -61,7 +62,12 @@ class CompInfo:
 			self.metaComp = o
 			self.metaPar = self.metaComp.par
 		else:
-			self.comp = None
+			libInfo = LibraryInfo.findContainingLibraryOf(o)
+			if libInfo and libInfo.isComponent(o, checkMaster=False):
+				self._libInfo = libInfo
+				self.comp = o
+			else:
+				self.comp = None
 			self.metaComp = None
 			self.metaPar = None
 
@@ -69,27 +75,44 @@ class CompInfo:
 		return bool(self.comp)
 
 	@property
+	def hasMeta(self):
+		return bool(self.metaPar)
+
+	def _requireHasMeta(self):
+		if not self.hasMeta:
+			raise RuntimeError(f'Component {self.comp!r} does not have compMeta!')
+
+	@property
 	def opVersion(self):
-		return str(self.metaPar.Opversion)
+		return str(self.metaPar.Opversion) if self.hasMeta else None
 
 	@opVersion.setter
 	def opVersion(self, val):
+		self._requireHasMeta()
 		self.metaPar.Opversion = val if val is not None else ''
 
 	@property
 	def opType(self):
+		if not self.comp:
+			return None
+		if not self.hasMeta:
+			if self._libInfo:
+				return LibraryContext(self._libInfo.comp).generateOpId(self.comp)
+			return None
 		return str(self.metaPar.Optype)
 
 	@opType.setter
 	def opType(self, val):
+		self._requireHasMeta()
 		self.metaPar.Optype = val or ''
 
 	@property
 	def opStatus(self):
-		return str(self.metaPar.Opstatus)
+		return str(self.metaPar.Opstatus) if self.hasMeta else None
 
 	@opStatus.setter
 	def opStatus(self, val):
+		self._requireHasMeta()
 		self.metaPar.Opstatus = val or 'unset'
 
 	@property
@@ -97,26 +120,35 @@ class CompInfo:
 		"""
 		Short form of the name of the COMP type (not the COMP instance).
 		"""
+		if not self.comp:
+			return None
+		if not self.hasMeta:
+			return self.comp.name
 		t = self.opType
 		return t and t.rsplit('.', 1)[-1]
 
 	@property
 	def toxFile(self) -> 'Optional[str]':
+		if not self.comp:
+			return None
 		return self.comp.par.externaltox.eval() or None
 
 	@property
 	def helpDat(self) -> 'Optional[DAT]':
+		if not self.hasMeta:
+			return None
 		dat = op(self.metaPar.Helpdat) or self.comp.op('help')
 		if dat and dat.isDAT:
 			return dat
 
 	@helpDat.setter
 	def helpDat(self, val: 'Optional[DAT]'):
+		self._requireHasMeta()
 		self.metaPar.Helpdat = val or ''
 
 	@property
-	def helpUrl(self):
-		return str(self.metaPar.Helpurl)
+	def helpUrl(self) -> 'Optional[str]':
+		return str(self.metaPar.Helpurl) if self.hasMeta else None
 
 def _isCompMeta(o: 'OP'):
 	return bool(o) and o.isCOMP and o.name == 'componentMeta' and o.par['Hostop'] is not None
@@ -127,10 +159,38 @@ def _isLibraryMeta(o: 'OP'):
 def _isPackageMeta(o: 'OP'):
 	return bool(o) and o.isCOMP and o.name == 'packageMeta' and o.par['Hostop'] is not None
 
+def _getLibraryRootAndMeta(comp: 'COMP') -> 'Tuple[Optional[COMP], Optional[_LibraryMetaCompT]]':
+	if not comp or not comp.isCOMP:
+		return None, None
+	if _isLibraryMeta(comp):
+		libRoot: 'COMP' = comp.par.Hostop.eval()
+		if not libRoot or not libRoot.isCOMP:
+			return None, None
+		# noinspection PyTypeChecker
+		return libRoot, comp
+	metaComp = comp.op('libraryMeta')
+	if _isLibraryMeta(metaComp):
+		# noinspection PyTypeChecker
+		return comp, metaComp
+	return None, None
+
+def _isLibraryRoot(o: 'Union[OP, str, Par, Cell]'):
+	libRoot, metaComp = _getLibraryRootAndMeta(op(o))
+	return bool(libRoot and metaComp)
+
+def _findSelfOrAncestorLibraryRootAndMeta(comp: 'COMP') -> 'Tuple[Optional[COMP], Optional[_LibraryMetaCompT]]':
+	while comp and comp.isCOMP and comp is not root:
+		libRoot, metaComp = _getLibraryRootAndMeta(comp)
+		if libRoot:
+			return libRoot, metaComp
+		comp = comp.parent()
+	return None, None
+
 class PackageInfo:
 	comp: 'Optional[COMP]'
 	metaComp: 'Optional[_PackageMetaCompT]'
 	metaPar: 'Optional[_PackageMetaParsT]'
+	_libInfo: 'Optional[LibraryInfo]'
 
 	def __init__(self, o: 'Union[OP, str, Cell, Par]'):
 		o = op(o)
@@ -167,24 +227,30 @@ class LibraryInfo:
 	metaComp: 'Optional[_LibraryMetaCompT]'
 	metaPar: 'Optional[_LibraryMetaParsT]'
 
-	def __init__(self, o: 'Union[OP, str, Cell, Par]'):
-		o = op(o)
-		if not o or not o.isCOMP:
-			return
-		if _isLibraryMeta(o.op('libraryMeta')):
-			self.comp = o
-			# noinspection PyTypeChecker
-			self.metaComp = o.op('libraryMeta')
-			self.metaPar = self.metaComp.par
-		elif _isLibraryMeta(o):
-			self.comp = o.par.Hostop.eval()
-			# noinspection PyTypeChecker
-			self.metaComp = o
+	def __init__(
+			self, o: 'Union[OP, str, Cell, Par, None]',
+			libRoot: 'Optional[COMP]' = None,
+			metaComp: 'Optional[COMP]' = None,
+	):
+		if libRoot and metaComp:
+			self.comp = libRoot
+			self.metaComp = metaComp
 			self.metaPar = self.metaComp.par
 		else:
-			self.comp = None
-			self.metaComp = None
-			self.metaPar = None
+			o = op(o)
+			if not o or not o.isCOMP:
+				return
+			libRoot, metaComp = _getLibraryRootAndMeta(o)
+			self.comp = libRoot
+			self.metaComp = metaComp
+			self.metaPar = metaComp and metaComp.par
+
+	@classmethod
+	def findContainingLibraryOf(cls, o: 'Union[OP, str, Cell, Par]'):
+		libRoot, metaComp = _findSelfOrAncestorLibraryRootAndMeta(op(o))
+		if libRoot and metaComp:
+			return cls(None, libRoot=libRoot, metaComp=metaComp)
+		return None
 
 	def __bool__(self):
 		return bool(self.comp)
@@ -192,6 +258,44 @@ class LibraryInfo:
 	@property
 	def libraryName(self):
 		return str(self.metaPar.Libraryname)
+
+	@property
+	def packageRoot(self) -> 'Optional[COMP]':
+		if not self.metaPar:
+			return None
+		return self.metaPar.Packageroot.eval() or self.comp
+
+	@property
+	def componentTags(self) -> 'List[str]':
+		return tdu.split(self.metaPar.Componenttags) if self.metaPar else []
+
+	@property
+	def packageTags(self) -> 'List[str]':
+		return tdu.split(self.metaPar.Packagetags) if self.metaPar else []
+
+	@property
+	def libraryVersion(self):
+		return str(self.metaPar.Libraryversion) if self.metaPar else ''
+
+	def isPackage(self, comp: 'COMP'):
+		if not comp:
+			return False
+		tags = self.packageTags
+		if tags and not any(t in tags for t in comp.tags):
+			return False
+		return bool(PackageInfo(comp))
+
+	def isComponent(self, comp: 'COMP', checkMaster: bool = False):
+		if not comp:
+			return False
+		if checkMaster:
+			master = comp.par.clone.eval()
+			if master is not comp:
+				return False
+		tags = self.componentTags
+		if tags and not any(t in tags for t in comp.tags):
+			return False
+		return bool(CompInfo(comp))
 
 class LibraryContext:
 	metaComp: 'Optional[_LibraryMetaCompT]'
@@ -210,8 +314,6 @@ class LibraryContext:
 		if not metaCompOrRoot or not metaCompOrRoot.isCOMP:
 			return
 		self.libraryInfo = LibraryInfo(metaCompOrRoot)
-		if not self.libraryInfo:
-			return
 		self.libraryRoot = self.libraryInfo.comp
 		self.metaComp = self.libraryInfo.metaComp
 		self.metaPar = self.libraryInfo.metaPar
@@ -223,17 +325,15 @@ class LibraryContext:
 
 	@property
 	def packageRoot(self) -> 'Optional[COMP]':
-		if not self:
-			return None
-		return self.metaPar.Packageroot.eval() or self.libraryRoot
+		return self.libraryInfo.packageRoot
 
 	@property
 	def componentTags(self) -> 'List[str]':
-		return tdu.split(self.metaPar.Componenttags)
+		return self.libraryInfo.componentTags
 
 	@property
 	def packageTags(self) -> 'List[str]':
-		return tdu.split(self.metaPar.Packagetags)
+		return self.libraryInfo.packageTags
 
 	@property
 	def libraryName(self):
@@ -241,7 +341,7 @@ class LibraryContext:
 
 	@property
 	def libraryVersion(self):
-		return str(self.metaPar.Libraryversion)
+		return self.libraryInfo.libraryVersion
 
 	def resolvePath(self, path: str) -> 'Optional[COMP]':
 		if not self or not path:
@@ -264,24 +364,10 @@ class LibraryContext:
 		self.callbacks.DoCallback(name, info)
 
 	def isPackage(self, comp: 'COMP'):
-		if not comp:
-			return False
-		tags = self.packageTags
-		if tags and not any(t in tags for t in comp.tags):
-			return False
-		return bool(PackageInfo(comp))
+		return self.libraryInfo.isPackage(comp)
 
 	def isComponent(self, comp: 'COMP', checkMaster: bool = False):
-		if not comp:
-			return False
-		if checkMaster:
-			master = comp.par.clone.eval()
-			if master is not comp:
-				return False
-		tags = self.componentTags
-		if tags and not any(t in tags for t in comp.tags):
-			return False
-		return bool(CompInfo(comp))
+		return self.libraryInfo.isComponent(comp, checkMaster=checkMaster)
 
 	def getPackage(self, comp: 'COMP', checkParents: bool) -> 'Optional[COMP]':
 		if not comp or comp is root:
@@ -300,22 +386,21 @@ class LibraryContext:
 			return self.getComponent(comp.parent(), checkParents=True)
 
 	def isWithinPackageRoot(self, comp: 'COMP'):
-		if not comp:
-			return False
-		packageRoot = self.packageRoot
-		return comp.path.startswith(packageRoot.path + '/')
+		return comp.path.startswith(self.packageRoot.path + '/') if comp else None
 
 	def validateAndGetPackageInfo(self, comp: 'COMP') -> PackageInfo:
 		if not self.isPackage(comp):
-			raise Exception(f'Invalid package: {comp}')
+			raise ValueError(f'Invalid package: {comp}')
 		return PackageInfo(comp)
 
 	def validateAndGetCompInfo(self, comp: 'COMP') -> CompInfo:
 		if not self.isComponent(comp, checkMaster=True):
-			raise Exception(f'Invalid component: {comp}')
+			raise ValueError(f'Invalid component: {comp}')
 		return CompInfo(comp)
 
 	def generateOpId(self, comp: 'COMP'):
+		if not self.isWithinPackageRoot(comp):
+			return None
 		path = self.packageRoot.relativePath(comp).strip('./')
 		return self.libraryInfo.libraryName + '.' + path.replace('/', '.')
 
