f354055de937032f5cc957d49bb5c1eaa225c0b3
t3kt <tekt@immerse.studio>
2025-03-30 14:02:52 -0700

    updating to TD 2023.12120

--- infraTools/navigationPanel/listCallbacks.py
+++ infraTools/navigationPanel/listCallbacks.py
@@ -1,11 +1,11 @@
-﻿from infraCommon import navigateTo
+from infraCommon import navigateTo
 
 # noinspection PyUnreachableCode
 if False:
 	# noinspection PyUnresolvedReferences
 	from _stubs import *
 
-def onClick(info):
+def onSelectRow(info):
 	print(info)
 	data = info['rowData']
 	path = data['rowObject']['path']
